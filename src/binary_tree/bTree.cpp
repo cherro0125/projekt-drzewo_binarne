b6beaee81388a29708f4162076859c22f00d8d94
Cherro <kaliszewski.dominik@gmail.com>
2018-04-09 22:55:33 +0200

    Code refactor

---
+++ src/binary_tree/bTree.cpp
@@ -0,0 +1,114 @@
+#include "bTree.h"
+
+
+bTree::bTree()
+{
+	root = NULL;
+}
+
+
+bTree::~bTree()
+{
+	destroy(root);
+}
+
+void bTree::insert(int val)
+{
+	if (root != NULL)
+		insert(val, root);
+	else
+	{
+		root = new node;
+		root->key = val;
+		root->Left = NULL;
+		root->Right = NULL;
+	}
+}
+
+void bTree::inorder(node *root)
+{
+	if (root != NULL)
+	{
+		inorder(root->Left);
+		std::cout << root->key << " ";
+		inorder(root->Right);
+	}
+}
+
+
+void bTree::destroy()
+{
+	destroy(root);
+}
+
+bool bTree::search(int val)
+{
+	return search(val, root);
+}
+
+void bTree::inorder()
+{
+	inorder(root);
+}
+
+
+
+void bTree::insert(int val, node * leaf)
+{
+	if (val < leaf->key)
+	{
+		if (leaf->Left != NULL)
+			insert(val, leaf->Left);
+		else
+		{
+			leaf->Left = new node;
+			leaf->Left->key = val;
+			leaf->Left->Left = NULL;
+			leaf->Left->Right = NULL;
+		}
+	}
+	else if (val >= leaf->key)
+	{
+		if (leaf->Right != NULL)
+			insert(val, leaf->Right);
+		else
+		{
+			leaf->Right = new node;
+			leaf->Right->key = val;
+			leaf->Right->Left = NULL;
+			leaf->Right->Right = NULL;
+		}
+	}
+}
+
+void bTree::destroy(node *leaf)
+{
+	if (leaf != NULL)
+	{
+		destroy(leaf->Left);
+		destroy(leaf->Right);
+		delete leaf;
+	}
+}
+
+bool bTree::search(int val, node *leaf)
+{
+	if (leaf != NULL)
+	{
+		if (val == leaf->key)
+			return true;
+		if (val<leaf->key)
+			return search(val, leaf->Left);
+		else
+			return search(val, leaf->Right);
+	}
+	else return false;
+}
+
+int node::get_val()
+{
+	if (this != NULL)
+		return this->key;
+	std::cout << "Wartosc nie wystepuje w drzewie!\n";
+	return -1;
+}
---
+++ src/binary_tree/binary_tree.cpp
@@ -0,0 +1,36 @@
+// binary_tree.cpp : Defines the entry point for the console application.
+//
+
+
+#include <iostream>
+#include "bTree.h"
+#include <locale>
+#include <string>
+#include <locale.h>
+
+
+
+using namespace std;
+
+int main()
+{
+
+	bTree *tree = new bTree;
+	tree->insert(2);
+	tree->insert(3);
+	tree->insert(1);
+	tree->insert(7);
+	tree->insert(92);
+	tree->insert(18);
+	tree->inorder();
+	const int searchVal = 3;
+	cout << endl;
+	if (tree->search(searchVal))
+		cout << "Znaleziono wartosc " << searchVal << " w drzewie" << endl;
+	else
+		cout << "Nie znaleziono wartosci " << searchVal << " w drzewie" << endl;
+
+	getchar();
+	return 0;
+}
+
