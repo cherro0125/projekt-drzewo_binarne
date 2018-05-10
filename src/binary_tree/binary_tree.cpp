61094d87fe4011b0580ca06b0d6ca12ae7006b48
karolkol <karolkolodz@gmail.com>
2018-05-10 20:04:42 +0200

    Add delete method to menu

--- src/binary_tree/binary_tree.cpp
+++ src/binary_tree/binary_tree.cpp
@@ -29,10 +29,11 @@ void print_main_menu_txt()
 	cout << "-----------------------------------------------------------\n";
 	cout << "                 1. MULTIPLY ADD NUMBERS\n";
 	cout << "                 2. ADD NUMBER\n";
-	cout << "                 3. DESTROY TREE\n";
-	cout << "                 4. SHOW TREE\n";
-	cout << "                 5. TREE EMPTY CHECK\n";
-	cout << "                 6. SEARCH NUMBER IN TREE\n";
+	cout << "                 3. DELETE NUMBER\n";
+	cout << "                 4. DESTROY TREE\n";
+	cout << "                 5. SHOW TREE\n";
+	cout << "                 6. TREE EMPTY CHECK\n";
+	cout << "                 7. SEARCH NUMBER IN TREE\n";
 	cout << "                 0. EXIT\n";
 	cout << "-----------------------------------------------------------\n";
 }
@@ -42,6 +43,7 @@ void print_menu()
 	
 	bTree *tree = new bTree;
 	int input;
+	int value;
 	do
 	{
 		print_main_menu_txt();
@@ -55,12 +57,29 @@ void print_menu()
 			tree->add_single_num();
 			break;
 		case 3:
+			if (!tree->empty())
+			{
+				cout << "What number do you want to delete??\n";
+				cin >> value;
+				tree->remove(value);
+				tree->clearDisplay();
+				tree->inorder();
+			}
+			else
+			{
+				setConsoleColor(GUI::MessageType::T_ERROR);
+				cout << "TREE IS EMPTY!\n";
+				setConsoleColor(GUI::MessageType::T_NORMAL);
+				Sleep(1000);
+			}
+			break;
+		case 4:
 			tree->destroy();
 			tree->clearDisplay();
 			tree->removeDisplay();
 			tree = new bTree;
 			break;
-		case 4:
+		case 5:
 			if (tree->empty())
 			{
 				setConsoleColor(GUI::MessageType::T_ERROR);
@@ -74,7 +93,7 @@ void print_menu()
 				tree->inorder();
 			}
 			break;
-		case 5:
+		case 6:
 			if (tree->empty())
 			{
 				setConsoleColor(GUI::MessageType::T_ERROR);
@@ -90,7 +109,7 @@ void print_menu()
 			}
 			Sleep(1200);
 			break;
-		case 6:
+		case 7:
 			tree->searchNum();
 			break;
 		case 0:
